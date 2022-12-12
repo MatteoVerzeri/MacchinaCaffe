67239fff70d6a2a9b4cfd94f5ba1fc3a06e7445e
MatteoVerzeri <matverzer@gmail.com>
2022-12-12 10:41:17 +0100

    Aggiungere i file di progetto.

---
+++ MacchinaCaffe/Caffe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacchinaCaffe
+{
+    public class Caffe
+    {
+        private string _id;
+        private string _nome;
+        private float _prezzo;
+        private int _contzucchero;
+        public string Id { get { return _id; } private set { _id = value; } }
+        public int contZucchero {  get { return _contzucchero;} private set { _contzucchero = value; } }
+        public string nome { get { return _nome; } private set { _nome = value; } }
+        public float prezzo { get { return _prezzo; } private set { _prezzo = value; } }
+        public Caffe(string nome, float prezzo, string id)
+        {
+            this.nome = nome;
+            this.prezzo = prezzo;
+            this._id = id;
+        }
+        public void aumentaZucchero()
+        {
+            contZucchero++;
+        }
+    }
+}
---
+++ MacchinaCaffe/Distributore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacchinaCaffe
+{
+    public class Distributore
+    {
+        private float _credito;
+        private string _id;
+        private Caffe[] _caffe;
+        private bool occupato;
+        public float credito { get { return _credito; } private set { _credito = value; } }
+        public string id { get { return _id; } private set { _id = value; } }
+        public Caffe[] caffe { get { return _caffe; } private set { _caffe = value; } }
+        
+        public Distributore(string id, Caffe[] caffe)
+        {
+            credito = 0;
+            this.id = id;
+            this.caffe = caffe;
+            occupato = false;
+        }   
+        public void acquistaCaffe(int selezione)
+        {
+            
+        }
+
+    }
+}
