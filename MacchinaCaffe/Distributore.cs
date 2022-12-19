a79239130f99a0f8c4b9cad4ed2d3ea728228c8b
MatteoVerzeri <matverzer@gmail.com>
2022-12-19 08:26:25 +0100

    funzioni + parte del main

--- MacchinaCaffe/Distributore.cs
+++ MacchinaCaffe/Distributore.cs
@@ -12,6 +12,7 @@ namespace MacchinaCaffe
         private string _id;
         private Caffe[] _caffe;
         private bool occupato;
+        private int qcaffe;
         public float credito { get { return _credito; } private set { _credito = value; } }
         public string id { get { return _id; } private set { _id = value; } }
         public Caffe[] caffe { get { return _caffe; } private set { _caffe = value; } }
@@ -22,11 +23,40 @@ namespace MacchinaCaffe
             this.id = id;
             this.caffe = caffe;
             occupato = false;
+            qcaffe = 10;
         }   
-        public void acquistaCaffe(int selezione)
+        public void acquistaCaffe(Caffe selezione)
         {
-            
+            if(credito >= selezione.prezzo)
+            {
+                erogazione();
+            }
         }
-
+        public void erogazione()
+        {
+            if ((qcaffe - 1) >= 0)
+            {
+                qcaffe--;
+                credito--;
+            }
+        }
+        public void ricaricacaffe(int qricarica)
+        {
+            if(qricarica >= 0 && (qcaffe + qricarica)<=0 )
+            qcaffe++;
+        }
+        public void ricaricacredito(int qcredito)
+        {
+            if(qcredito >= 0)
+            credito = credito + qcredito;
+        }
+        /*public string visualizza()
+        {
+            string visualizza;
+            for (int i = 0; i < caffe.Length; i++)
+            {
+                visualizza = 
+            }
+        }*/
     }
 }
